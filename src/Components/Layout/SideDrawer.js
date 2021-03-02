663c6ea32781c3a8b485a55e970376b73ae5ebb8
Amr Elzafarani <amrelza3frany@gmail.com>
2021-03-02 22:39:43 +0200

    Inserting data into sideDrawer

--- src/Components/Layout/SideDrawer.js
+++ src/Components/Layout/SideDrawer.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Nav } from "react-bootstrap";
 
-class SideDrawer extends Component {
-    render() {
-        return (
-            <div>Amr</div>
-        );
+import { SidebarData } from "./SidebarData";
+import "./SideDrawer.css";
 
-    }
-}
+import { Link } from "react-router-dom";
+
+const SideDrawer = (props) => {
+
+
+  return (
+
+    <>
+        <Nav >
+
+            <ul className={props.sidebar ? "nav-menu active" : "nav-menu"}>
+                <h2>Iconic</h2>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </Nav>
+    </>
+  );
+};
 
 export default SideDrawer;
+
